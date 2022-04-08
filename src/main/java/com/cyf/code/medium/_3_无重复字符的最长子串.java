6dcfffd8e32c4330c6cb00e1d6a2caa5f1ae9be2
leon <244604162@qq.com>
2022-04-08 22:58:16 +0800

    add

--- src/main/java/com/cyf/code/medium/_3_无重复字符的最长子串.java
+++ src/main/java/com/cyf/code/medium/_3_无重复字符的最长子串.java
@@ -65,21 +65,48 @@ public class _3_无重复字符的最长子串 {
 			return len;
 		}
 		for (int i = 0; i < len; i++) {
+
+			//获取当前的字符
 			char cur = s.charAt(i);
 
+			//找到左边界,如果一直没有重复，这左边界一直是0，如果这个字符之前出现过，这左边界就是出现字符下标的下一个字符
+			//比如 abcab,当第二个a出现时，左边界应该是第一个a的下一个字符b的下标
 			start = Math.max(index[cur], start);
 
+			//找到最大长度
 			res = Math.max(res, i - start + 1);
 
+			//存的是当前字符的下一个下标
 			index[cur] = i+1;
 		}
 		return res;
 	}
 
+	public static int lengthOfLongestSubstring3(String s) {
+
+		int [] pos = new int[128];
+		int max = 0;
+		int left = 0;
+
+		for (int i = 0; i < s.length(); i++){
+			char cur = s.charAt(i);
+
+			//找到左边界
+			left = Math.max(pos[cur]+1, left);
+
+			//计算最大长度
+			max = Math.max(max, i-left+1);
+
+			pos[cur] = i;
+		}
+
+		return max;
+	}
+
 
 	public static void main(String[] args) {
-		String s = "au";
-		int max = lengthOfLongestSubstring2(s);
+		String s = "abcabcd";
+		int max = lengthOfLongestSubstring3(s);
 
 		System.out.println("===max="+max);
 
